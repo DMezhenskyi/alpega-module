870a9bd96cc421c087751f9f12ae3a264e5a06c4
Dmytro Mezhenskyi <dmytro@decodedfrontend.io>
2022-04-27 10:51:49 +0200

    add directive for example

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { HighlightDirective } from './highlight.directive';
 
 @NgModule({
-  declarations: [AppComponent, HeaderComponent],
+  declarations: [AppComponent, HeaderComponent, HighlightDirective],
   imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent]
